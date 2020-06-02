77bdcdf9e892e05ea82c89cc101d13dbdb8b5c41
Aditya baghel <adityabaghel1228@gmail.com>
2020-06-02 19:16:27 +0530

    files added to project

---
+++ app.js
@@ -0,0 +1,38 @@
+var express=require('express');
+var app = express();
+var fs= require('fs');
+
+app.get('/video', function(req, res) {
+    const path = 'sample.mp4'
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+  });
+
+app.listen(5000,()=>{
+    console.log("hit url localhOST:5000/video or embedd in video tag")
+})
